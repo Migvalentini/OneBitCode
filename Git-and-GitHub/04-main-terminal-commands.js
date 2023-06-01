a40027aadc9d858f041d2a180d620b96c3c89590
Migvalentini <miguelvalentini2006@gmail.com>
2023-05-31 21:47:53 -0300

    Create 04-main-terminal-commands.js

---
+++ Git-and-GitHub/04-main-terminal-commands.js
@@ -0,0 +1,18 @@
+/* 
+echo 'Hello World' = print a message
+pwd = show yhe file path
+ls = show the elements inside the directory
+mkdir [name] = create a new folder
+cd [name] = navigate to another folder
+touch [name] = create or update a file
+cat [name] = get the contents of the file
+less [name] = display the contents of the file
+cp [name] [name] = copy a file to another file
+mv [name] [name] = move the contents of a file to another file
+rn [name] = remove a file
+clear = clear the terminal
+history = show command history
+! [number] = return a command from the history list
+grep = filter the history command
+exit = exit terminal
+*/
